6b05c22867348cb0a35236023502acc49b658965
Karim <jaouane.karim@gmail.com>
2018-11-20 17:13:48 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import { NewPostComponent } from './new-post/new-post.component';
+import {Routes, RouterModule} from '@angular/router'
+import {  ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+
+const appRoutes: Routes = [
+  { path: 'posts',  component: PostListComponent },
+  { path: 'new', component: NewPostComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  
+];
 
 
 @NgModule({
@@ -16,10 +24,14 @@ import { NewPostComponent } from './new-post/new-post.component';
     PostListItemComponent,
     NewPostComponent,
    
+   
     
   ],
   imports: [
     BrowserModule ,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
